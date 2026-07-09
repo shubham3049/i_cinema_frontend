549186580361049bd32108abd117b8bcb5dcb293
somesh2387 <somesh.kumar@edcast.com>
2026-07-09 23:31:13 +0530

    Initial React TypeScript Project

---
+++ src/types/Movie.ts
@@ -0,0 +1,10 @@
+export interface Movie{
+    movieID :number;
+    movieName:string;
+    genre:string;
+    language:string;
+    rating:number;
+    releaseDate:number;
+    description:string;
+    imageUrl:string;
+}
